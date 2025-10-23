4c6cae3331e70c85e2396c0c82458620804548c6
root <root@vmi2545422.contaboserver.net>
2025-10-23 21:08:53 +0200

    add mkp

--- src/api/controllers/mkp.controller.js
+++ src/api/controllers/mkp.controller.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const mkpcontroller = async (req, res) => {
     console.log('=============================================');
     console.log('[MKPController] ⚡ Petición RECIBIDA para MKP');
-    console.log(req.query);
+    console.log(req.body);
 
 
 };
